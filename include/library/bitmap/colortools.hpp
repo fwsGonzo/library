3c0d5dc7edc6ea54f598ba1baf2735d11898461b
fwsgonzo <fwsgonzo@hotmail.com>
2014-06-16 21:09:11 +0200

    Fixed RGBA8 & BGRA8

--- include/library/bitmap/colortools.hpp
+++ include/library/bitmap/colortools.hpp
@@ -10,8 +10,7 @@ namespace library
 	class vec4;
 	typedef uint32_t rgba8_t;
 	
-	template <class C8, class C_8>
-	rgba8_t BGRA8(C8 r, C8 g, C8 b, C_8 a)
+	rgba8_t BGRA8(int r, int g, int b, int a)
 	{
 		return  ((rgba8_t)r      ) + 
 				((rgba8_t)g <<  8) + 
@@ -19,8 +18,7 @@ namespace library
 				((rgba8_t)a << 24);
 	}
 	
-	template <class C8, class C_8>
-	rgba8_t RGBA8(C8 r, C8 g, C8 b, C_8 a)
+	rgba8_t RGBA8(int r, int g, int b, int a)
 	{
 		return  ((rgba8_t)b      ) + 
 				((rgba8_t)g <<  8) + 
