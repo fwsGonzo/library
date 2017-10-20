53906a2dcdc13695fda3b6b7f8b48a56c90896b9
fwsGonzo <fwsgonzo@hotmail.com>
2017-10-20 16:37:53 +0200

    Add back look-vector formula

--- include/library/math/vector.hpp
+++ include/library/math/vector.hpp
@@ -3,3 +3,14 @@
 #include <glm/vec2.hpp>
 #include <glm/vec3.hpp>
 #include <glm/vec4.hpp>
+#include <cmath>
+
+namespace library
+{
+  inline glm::vec3 lookVector(const glm::vec2& rot)
+  {
+  		return glm::vec3( std::sin(rot.y) * std::cos(rot.x),
+  								     -std::sin(rot.x),
+  					           -std::cos(rot.y) * std::cos(rot.x));
+  }
+}
