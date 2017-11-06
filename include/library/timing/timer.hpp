3d9ea0ab8ce691712362eb74649f883d1aad1552
fwsGonzo <fwsgonzo@hotmail.com>
2017-11-06 21:28:32 +0100

    Remove highperf timer abstraction

--- include/library/timing/timer.hpp
+++ include/library/timing/timer.hpp
@@ -1,7 +1,7 @@
 #ifndef LIBRARY_TIMER_HPP
 #define LIBRARY_TIMER_HPP
 
-#include "highperf.hpp"
+#include <chrono>
 
 namespace library
 {
@@ -9,21 +9,52 @@ namespace library
 	{
 	public:
 		typedef double time_t;
-		
+
 		//! \brief constructor sets t0 to the current time
-		Timer();
-		
+		inline Timer();
+
 		//! \brief returns the time (in fractional seconds) between the last getDeltaTime()
 		//! function call, or time since object was instantiated (whichever happened last)
-		time_t getTime();
-		
-		//! \brief returns time elapsed, and sets new time, so 
+		inline time_t getTime();
+
+		//! \brief returns time elapsed, and sets new time, so
 		//! all future rounds and deltas are calculated from new time point
-		time_t restart();
-		
+		inline time_t restart();
+
 	private:
-		HighResClock::time_point t0;
+		std::chrono::steady_clock::time_point t0;
 	};
+
+  inline Timer::Timer()
+	{
+		// the starting time, which is updated at each "interval"
+		t0 = std::chrono::high_resolution_clock::now();
+	}
+
+	// returns time elapsed in high-precision seconds
+	Timer::time_t Timer::getTime()
+	{
+    using namespace std::chrono;
+		auto t1 = high_resolution_clock::now();
+		auto micros = duration_cast<microseconds>(t1 - t0);
+
+		// return high-precision seconds
+		return micros.count() / 1e6f;
+	}
+
+	// defines an interval change, and returns the time between last call
+	// in high-precision seconds
+	Timer::time_t Timer::restart()
+	{
+    using namespace std::chrono;
+		auto t1 = high_resolution_clock::now();
+		auto micros = duration_cast<microseconds>(t1 - t0);
+
+		// set last time = this time
+		t0 = t1;
+		// return high-precision seconds
+		return micros.count() / 1e6f;
+	}
 }
 
 #endif
