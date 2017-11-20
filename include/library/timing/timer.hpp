6a0f8212b7319adc5c585c8c4d9d625069759f72
fwsGonzo <fwsgonzo@hotmail.com>
2017-11-20 18:55:25 +0100

    Add rolling average timing helper

--- include/library/timing/timer.hpp
+++ include/library/timing/timer.hpp
@@ -1,5 +1,5 @@
-#ifndef LIBRARY_TIMER_HPP
-#define LIBRARY_TIMER_HPP
+#ifndef LIBRARY_TIMING_TIMER_HPP
+#define LIBRARY_TIMING_TIMER_HPP
 
 #include <chrono>
 
@@ -13,13 +13,11 @@ namespace library
 		//! \brief constructor sets t0 to the current time
 		inline Timer();
 
-		//! \brief returns the time (in fractional seconds) between the last getDeltaTime()
-		//! function call, or time since object was instantiated (whichever happened last)
+		//! \brief returns the time (in seconds) since construction
 		inline time_t getTime() const;
 
-		//! \brief returns time elapsed, and sets new time, so
-		//! all future rounds and deltas are calculated from new time point
-		inline time_t restart();
+		//! \brief sets new time t0
+		inline void restart();
 
 	private:
 		std::chrono::steady_clock::time_point t0;
@@ -44,16 +42,10 @@ namespace library
 
 	// defines an interval change, and returns the time between last call
 	// in high-precision seconds
-	Timer::time_t Timer::restart()
+	void Timer::restart()
 	{
     using namespace std::chrono;
-		auto t1 = high_resolution_clock::now();
-		auto micros = duration_cast<microseconds>(t1 - t0);
-
-		// set last time = this time
-		t0 = t1;
-		// return high-precision seconds
-		return micros.count() / 1e6f;
+		t0 = high_resolution_clock::now();
 	}
 }
 
