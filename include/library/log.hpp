197c1d816b6c0ffed1a92a66affc988b00501e28
Alf-André Walla <fwsgonzo@hotmail.com>
2020-05-16 20:15:06 +0200

    Add void* printing to logger

--- include/library/log.hpp
+++ include/library/log.hpp
@@ -53,17 +53,29 @@ private:
     friend Log& operator<<(Log& out, const LogLevel level);
 	friend Log& operator<<(Log& out, const std::string& str);
 	friend Log& operator<<(Log& out, const char* str);
+	friend Log& operator<<(Log& out, void* const ptr);
     template <class T>
     friend Log& operator<<(Log& out, const T t);
 };
 
 inline Log& operator<<(Log& out, const std::string& str)
 {
-    out.log += str;
+    out.log.append(str);
     return out;
 }
 inline Log& operator<<(Log& out, const char* str)
 {
+    out.log.append(str);
+    return out;
+}
+inline Log& operator<<(Log& out, void* const ptr)
+{
+	static const char* digits = "0123456789ABCDEF";
+	const size_t hex_len = sizeof(void*) * 2;
+	const uintptr_t val = (uintptr_t) ptr;
+	std::string str(hex_len, '0');
+	for (size_t i = 0, j = (hex_len-1)*4 ; i<hex_len; ++i, j -= 4)
+		str[i] = digits[(val >> j) & 0xF];
     out.log.append(str);
     return out;
 }
