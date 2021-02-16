f0e980ccfc7c600aa3fe0cfcc84af0dc570379eb
DrRemorse <DrRemorse@gmail.com>
2021-02-15 23:53:59 -0500

    voronoi

--- library/noise/voronoi.cpp
+++ library/noise/voronoi.cpp
@@ -62,10 +62,21 @@ inline unsigned int hash(unsigned int x)
     x = x ^ (x >> 15);
     return x;
 }
-inline int ihash(int x) { return hash(x) & INT32_MAX; }
 
-inline float randf(int x) { return ihash(x) / (float) INT32_MAX; }
-inline float randf(int x, int y) { return randf(x xor ihash(y)); }
+inline int ihash(int x)
+{
+    return hash(x) & INT32_MAX;
+}
+
+inline float randf(int x)
+{
+    return ihash(x) / (float)INT32_MAX;
+}
+
+inline float randf(int x, int y)
+{
+    return randf(x ^ ihash(y));
+}
 
 Voronoi::vor_t Voronoi::getdist(vor_t px, vor_t py, distance_func distFunc)
 {
