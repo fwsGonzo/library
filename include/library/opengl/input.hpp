0bbddc52ed2d270f239c3db4a7baa4cf38860d0f
Alf-André Walla <fwsgonzo@hotmail.com>
2022-03-08 03:13:27 +0100

    Fix line endings in input module

--- include/library/opengl/input.hpp
+++ include/library/opengl/input.hpp
@@ -12,89 +12,89 @@ namespace library
 class Input
 {
 public:
-    static const int MAX_KEYS = 512;
-    static const int MAX_MOUSE = 6;
+	static const int MAX_KEYS = 512;
+	static const int MAX_MOUSE = 6;
 
-    enum keystate_t
-    {
-        KEY_RELEASED,
-        KEY_PRESSED,
-        KEY_LOCKED
-    };
+	enum keystate_t
+	{
+		KEY_RELEASED,
+		KEY_PRESSED,
+		KEY_LOCKED
+	};
 
-    typedef int key_t;
+	typedef int key_t;
 
-    struct input_t
-    {
-        input_t() : action(0), mods(0){};
+	struct input_t
+	{
+		input_t() : action(0), mods(0){};
 
-        int action;
-        int mods;
-    };
+		int action;
+		int mods;
+	};
 
-    // initialize input using the current context
-    void init(GLFWwindow*, bool keyboard, bool mouse);
-    ~Input();
+	// initialize input using the current context
+	void init(GLFWwindow*, bool keyboard, bool mouse);
+	~Input();
 
-    // public keyboard functions
-    input_t key_ex(int key) const { return this->m_keys.at(key); }
-    key_t key(int key) const { return key_ex(key).action; }
-    // "holds" a key, allowing code to see that a single-fire event has been executed already
-    void key_hold(int key) { m_keys.at(key).action = KEY_LOCKED; }
-    // releases the hold of a key, setting state from KEY_LOCKED back to KEY_PRESSED
-    void key_release(int key)
-    {
-        if (m_keys.at(key).action == KEY_LOCKED) m_keys[key].action = KEY_PRESSED;
-    }
+	// public keyboard functions
+	input_t key_ex(int key) const { return this->m_keys.at(key); }
+	key_t key(int key) const { return key_ex(key).action; }
+	// "holds" a key, allowing code to see that a single-fire event has been executed already
+	void key_hold(int key) { m_keys.at(key).action = KEY_LOCKED; }
+	// releases the hold of a key, setting state from KEY_LOCKED back to KEY_PRESSED
+	void key_release(int key)
+	{
+		if (m_keys.at(key).action == KEY_LOCKED) m_keys[key].action = KEY_PRESSED;
+	}
 
-    // public mouse functions
-    // set mouse options
-    void mouse_options(double speed, double sensitivity);
-    // enable FPS-like mouse grabbing
-    void mouse_grab(bool grab);
-    // mouse position
-    glm::vec2 mouse_xy() const { return this->m_mouse_xy; }
-    input_t mouse_button_ex(int btn) const { return this->m_mouse.at(btn); }
-    key_t mouse_button(int btn) const { return this->m_mouse.at(btn).action; }
-    void mouse_hold(int btn) { this->m_mouse.at(btn).action = KEY_LOCKED; }
-    // mouse rotation functions
-    glm::vec2 rotation() const noexcept { return this->m_rot; }
-    void set_rotation(glm::vec2 rot) { this->m_rot = rot; }
-    void add_rotation(glm::vec2 rot) { this->m_rot += rot; }
-    void rotate_degrees(glm::vec2 degrees);
+	// public mouse functions
+	// set mouse options
+	void mouse_options(double speed, double sensitivity);
+	// enable FPS-like mouse grabbing
+	void mouse_grab(bool grab);
+	// mouse position
+	glm::vec2 mouse_xy() const { return this->m_mouse_xy; }
+	input_t mouse_button_ex(int btn) const { return this->m_mouse.at(btn); }
+	key_t mouse_button(int btn) const { return this->m_mouse.at(btn).action; }
+	void mouse_hold(int btn) { this->m_mouse.at(btn).action = KEY_LOCKED; }
+	// mouse rotation functions
+	glm::vec2 rotation() const noexcept { return this->m_rot; }
+	void set_rotation(glm::vec2 rot) { this->m_rot = rot; }
+	void add_rotation(glm::vec2 rot) { this->m_rot += rot; }
+	void rotate_degrees(glm::vec2 degrees);
 
-    // returns mousewheel status _AND_ resets it internally
-    int mouse_wheel() const; // 0 = no change, down < 0, up > 0
+	// returns mousewheel status _AND_ resets it internally
+	int mouse_wheel() const; // 0 = no change, down < 0, up > 0
 
-    void restore_keyboard();
-    void restore_mouse();
+	void restore_keyboard();
+	void restore_mouse();
 
-    static void keyboard(GLFWwindow*, int key, int action, int a, int b);
-    static void mouseMove(GLFWwindow*, double x, double y);
-    static void mouseButton(GLFWwindow*, int button, int action, int mods);
-    static void mouseWheel(GLFWwindow*, double x, double y);
+	static void keyboard(GLFWwindow*, int key, int action, int a, int b);
+	static void mouseMove(GLFWwindow*, double x, double y);
+	static void mouseButton(GLFWwindow*, int button, int action, int mods);
+	static void mouseWheel(GLFWwindow*, double x, double y);
 
 protected:
-    GLFWwindow* m_window = nullptr;
-    double m_speed;
-    double m_sensitivity;
-    glm::vec2 m_mouse_xy;
-    glm::vec2 m_last_mouse_xy;
+	GLFWwindow* m_window = nullptr;
+	double m_speed;
+	double m_sensitivity;
+	glm::vec2 m_mouse_xy;
+	glm::vec2 m_last_mouse_xy;
 
-    // pitch & yaw
-    glm::vec2 m_rot;
+	// pitch & yaw
+	glm::vec2 m_rot;
 
-    // keyboard keys
-    std::array<input_t, MAX_KEYS> m_keys;
-    // mouse buttons
-    std::array<input_t, MAX_MOUSE> m_mouse;
-    // mousewheel
-    mutable int m_wheel = 0;
-    // mouse grabbing mode
-    bool m_mousegrab = false;
+	// keyboard keys
+	std::array<input_t, MAX_KEYS> m_keys {};
+	// mouse buttons
+	std::array<input_t, MAX_MOUSE> m_mouse {};
+	// mousewheel
+	mutable int m_wheel = 0;
+	// mouse grabbing mode
+	bool m_mousegrab = false;
 
 private:
-    static Input* inputFromWindow(GLFWwindow*);
+	static Input* inputFromWindow(GLFWwindow*);
 };
 } // namespace library
 
--- library/opengl/input.cpp
+++ library/opengl/input.cpp
@@ -5,7 +5,6 @@
 #include <library/log.hpp>
 #include <library/math/toolbox.hpp>
 #include <library/opengl/opengl.hpp>
-#include <set>
 
 namespace library
 {
@@ -13,141 +12,139 @@ static Input* currentInput = nullptr;
 
 void Input::init(GLFWwindow* window, const bool kbd, const bool mouse)
 {
-    currentInput = this;
-    // set owning window
-    this->m_window = window;
-    this->m_speed = 0.12;
-    this->m_sensitivity = 8;
-    // default rotation
-    this->m_rot = glm::vec2(0.0f);
+	currentInput = this;
+	// set owning window
+	this->m_window = window;
+	this->m_speed = 0.12;
+	this->m_sensitivity = 8;
+	// default rotation
+	this->m_rot = glm::vec2(0.0f);
 
-    if (kbd) { restore_keyboard(); }
-    if (mouse) { restore_mouse(); }
+	if (kbd) { restore_keyboard(); }
+	if (mouse) { restore_mouse(); }
 }
 void Input::restore_keyboard()
 {
-    // hook keyboard events
-    glfwSetKeyCallback(m_window, &Input::keyboard);
-    // disable sticky keys
-    glfwSetInputMode(m_window, GLFW_STICKY_KEYS, GL_TRUE);
+	// hook keyboard events
+	glfwSetKeyCallback(m_window, &Input::keyboard);
+	// disable sticky keys
+	glfwSetInputMode(m_window, GLFW_STICKY_KEYS, GL_TRUE);
 }
 void Input::restore_mouse()
 {
-    // hook mouse events
-    glfwSetCursorPosCallback(m_window, &Input::mouseMove);
-    // mouse button event
-    glfwSetMouseButtonCallback(m_window, &Input::mouseButton);
-    // mouse wheel event
-    glfwSetScrollCallback(m_window, &Input::mouseWheel);
+	// hook mouse events
+	glfwSetCursorPosCallback(m_window, &Input::mouseMove);
+	// mouse button event
+	glfwSetMouseButtonCallback(m_window, &Input::mouseButton);
+	// mouse wheel event
+	glfwSetScrollCallback(m_window, &Input::mouseWheel);
 }
 Input::~Input() { currentInput = nullptr; }
 
 void Input::mouse_options(double speed, double sensitivity)
 {
-    this->m_speed = speed;
-    this->m_sensitivity = sensitivity;
+	this->m_speed = speed;
+	this->m_sensitivity = sensitivity;
 }
 void Input::mouse_grab(const bool grab)
 {
-    this->m_mousegrab = grab;
-    if (grab)
-        glfwSetInputMode(m_window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
-    else
-        glfwSetInputMode(m_window, GLFW_CURSOR, GLFW_CURSOR_NORMAL);
+	this->m_mousegrab = grab;
+	if (grab)
+		glfwSetInputMode(m_window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+	else
+		glfwSetInputMode(m_window, GLFW_CURSOR, GLFW_CURSOR_NORMAL);
 }
 
 int Input::mouse_wheel() const
 {
-    int wheel = this->m_wheel;
-    this->m_wheel = 0;
-    return wheel;
+	int wheel = this->m_wheel;
+	this->m_wheel = 0;
+	return wheel;
 }
 
 void Input::keyboard(GLFWwindow* window, int key, int, int action, int mods)
 {
-    auto* input = Input::inputFromWindow(window);
-    if (input)
-    {
-        // action: GLFW_PRESS, GLFW_REPEAT or GLFW_RELEASE
-        if (action == GLFW_PRESS || action == GLFW_REPEAT)
-        {
-            // as long as the key isnt force-held, set to pressed
-            if (input->m_keys[key].action != Input::KEY_LOCKED)
-                input->m_keys[key].action = Input::KEY_PRESSED;
-        } // key is no longer pressed
-        else
-            input->m_keys[key].action = Input::KEY_RELEASED;
-        // keypress modifiers: SHIFT, CTRL, ALT
-        input->m_keys[key].mods = mods;
-    }
+	auto* input = Input::inputFromWindow(window);
+	assert(input != nullptr);
+	// action: GLFW_PRESS, GLFW_REPEAT or GLFW_RELEASE
+	if (action == GLFW_PRESS || action == GLFW_REPEAT)
+	{
+		// as long as the key isnt force-held, set to pressed
+		if (input->m_keys[key].action != Input::KEY_LOCKED)
+			input->m_keys[key].action = Input::KEY_PRESSED;
+	} // key is no longer pressed
+	else
+		input->m_keys[key].action = Input::KEY_RELEASED;
+	// keypress modifiers: SHIFT, CTRL, ALT
+	input->m_keys[key].mods = mods;
 }
 
 void Input::rotate_degrees(glm::vec2 degrees)
 {
-    static const float PI = 4 * atan(1);
-    static const float degToRad = PI / 180;
-    static const float maxX = 89 * degToRad;
-    static const float maxY = PI * 2.0;
+	static const float PI = 4 * atan(1);
+	static const float degToRad = PI / 180;
+	static const float maxX = 89 * degToRad;
+	static const float maxY = PI * 2.0;
 
-    this->m_rot += degrees * degToRad;
-    // clamping
-    if (this->m_rot.x > maxX) this->m_rot.x = maxX;
-    if (this->m_rot.x < -maxX) this->m_rot.x = -maxX;
-    while (this->m_rot.y < 0) this->m_rot.y += PI * 2;
-    while (this->m_rot.y >= maxY) this->m_rot.y -= PI * 2;
+	this->m_rot += degrees * degToRad;
+	// clamping
+	if (this->m_rot.x > maxX) this->m_rot.x = maxX;
+	if (this->m_rot.x < -maxX) this->m_rot.x = -maxX;
+	while (this->m_rot.y < 0) this->m_rot.y += PI * 2;
+	while (this->m_rot.y >= maxY) this->m_rot.y -= PI * 2;
 }
 
 void Input::mouseMove(GLFWwindow* window, double x, double y)
 {
-    Input* input = Input::inputFromWindow(window);
-    assert(input != nullptr);
-    if (input->m_mousegrab)
-    {
-        // in-game
-        double dx = (x - input->m_last_mouse_xy.x) * input->m_speed;
-        double dy = (y - input->m_last_mouse_xy.y) * input->m_speed;
+	Input* input = Input::inputFromWindow(window);
+	assert(input != nullptr);
+	if (input->m_mousegrab)
+	{
+		// in-game
+		double dx = (x - input->m_last_mouse_xy.x) * input->m_speed;
+		double dy = (y - input->m_last_mouse_xy.y) * input->m_speed;
 
-        if (fabs(dx) > input->m_sensitivity)
-            dx = signum(dx) * input->m_sensitivity + dx / input->m_sensitivity;
-        if (fabs(dy) > input->m_sensitivity)
-            dy = signum(dy) * input->m_sensitivity + dy / input->m_sensitivity;
+		if (fabs(dx) > input->m_sensitivity)
+			dx = signum(dx) * input->m_sensitivity + dx / input->m_sensitivity;
+		if (fabs(dy) > input->m_sensitivity)
+			dy = signum(dy) * input->m_sensitivity + dy / input->m_sensitivity;
 
-        // rotation on axes
-        input->rotate_degrees(glm::vec2(dy, dx));
+		// rotation on axes
+		input->rotate_degrees(glm::vec2(dy, dx));
 
-        // remember the last pos >:|
-        input->m_last_mouse_xy = {x, y};
-    }
-    else
-    {
-        input->m_last_mouse_xy.x = input->m_mouse_xy.x;
-        input->m_last_mouse_xy.y = input->m_mouse_xy.y;
-    }
-    input->m_mouse_xy = {x, y};
+		// remember the last pos >:|
+		input->m_last_mouse_xy = {x, y};
+	}
+	else
+	{
+		input->m_last_mouse_xy.x = input->m_mouse_xy.x;
+		input->m_last_mouse_xy.y = input->m_mouse_xy.y;
+	}
+	input->m_mouse_xy = {x, y};
 }
 
 void Input::mouseButton(GLFWwindow* window, int button, int action, int mods)
 {
 #define MACTION() (action == GLFW_PRESS) ? Input::KEY_PRESSED : Input::KEY_RELEASED
 
-    auto* input = Input::inputFromWindow(window);
-    assert(input != nullptr);
-    input->m_mouse.at(button).action = MACTION();
-    input->m_mouse.at(button).mods = mods;
+	auto* input = Input::inputFromWindow(window);
+	assert(input != nullptr);
+	input->m_mouse.at(button).action = MACTION();
+	input->m_mouse.at(button).mods = mods;
 }
 
 void Input::mouseWheel(GLFWwindow* window, double, double y)
 {
-    Input* input = Input::inputFromWindow(window);
-    if (input)
-    {
-        if (y < 0)
-            input->m_wheel -= 1;
-        else if (y > 0)
-            input->m_wheel += 1;
-        else
-            input->m_wheel = 0;
-    }
+	Input* input = Input::inputFromWindow(window);
+	if (input)
+	{
+		if (y < 0)
+			input->m_wheel -= 1;
+		else if (y > 0)
+			input->m_wheel += 1;
+		else
+			input->m_wheel = 0;
+	}
 }
 
 Input* Input::inputFromWindow(GLFWwindow*) { return currentInput; }
