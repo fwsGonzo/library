f16306db5303d4f14cdeff9c57dc9494308e6029
Alf-André Walla <fwsgonzo@hotmail.com>
2025-02-11 12:22:35 +0100

    Improve config template get

--- include/library/config.hpp
+++ include/library/config.hpp
@@ -1,8 +1,7 @@
 #ifndef LIBRARY_CONFIG_HPP
 #define LIBRARY_CONFIG_HPP
 
-#include <map>
-#include <sstream>
+#include <unordered_map>
 #include <string>
 
 namespace library
@@ -14,35 +13,20 @@ public:
     Config(const std::string& file);
     bool load(const std::string& file);
 
-    // std::string
-    std::string get(const std::string& var, const std::string& def)
-    {
-        // not found in hashmap -> return default
-        if (kv.find(var) == kv.end()) return def;
-        // return result
-        return kv[var];
-    }
-    // const char*
-    std::string get(const std::string& var, const char* def)
-    {
-        // not found in hashmap -> return default
-        if (kv.find(var) == kv.end()) return def;
-        // return result
-        return kv[var];
-    }
-    // algebraic
     template <typename T>
-    T get(const std::string& var, const T def)
+    auto get(const std::string& var, const T def)
     {
-        // if variable is not found in hashmap, return default
-        if (kv.find(var) == kv.end()) return def;
-        // initialize result to default value
-        T result;
-        std::stringstream convert(kv[var]);
-        // if the conversion fails, return default
-        if (!(convert >> result)) return def;
-        // return converted type T
-        return result;
+        auto it = kv.find(var);
+        if (it == kv.end()) {
+			if constexpr (std::is_same_v<T, const char*>)
+				return std::string(def);
+			else
+				return def;
+		}
+		if constexpr (std::is_same_v<std::remove_cvref_t<T>, std::string> || std::is_same_v<T, const char*>)
+			return it->second;
+		else
+			return static_cast<T>(std::stod(it->second));
     }
 
     void set(const std::string& key, const std::string& value)
@@ -59,7 +43,7 @@ public:
     }
 
 private:
-    std::map<std::string, std::string> kv;
+    std::unordered_map<std::string, std::string> kv;
 };
 } // namespace library
 
